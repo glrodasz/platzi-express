f7a365fd031ccc3562f2a250a849a9e0fd23dd71
Guillermo Rodas <garethderioth@gmail.com>
2018-11-23 12:34:01 -0500

    NPM scripts

--- ecommerce/routes/views/products.js
+++ ecommerce/routes/views/products.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const ProductsService = require("../../services/products");
+const { config } = require("../../config");
 
 const productService = new ProductsService();
 
@@ -8,7 +9,7 @@ router.get("/", async function(req, res, next) {
   const { tags } = req.query;
   try {
     const products = await productService.getProducts({ tags });
-    res.render("products", { products });
+    res.render("products", { products, dev: config.dev });
   } catch (err) {
     next(err);
   }
